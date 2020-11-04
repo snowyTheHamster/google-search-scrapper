ca528e869be0163b5b999db9d63ad7f4adbe941d
Taku <vidafuku@gmail.com>
2020-11-04 15:45:47 +0800

    added examples

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Python Search Result Analyzer
+# Google Search Engine Result Scrapper - Python
+
+Scrapes prepared search terms from a google search and saves it to a csv file.
+
+Run it manually or schedule it daily to track search results over time.
+
+### Installation
+
+- clone this repo in a project folder
+- make a virtual environment
+- install the requiremd modules
+- prepare search terms in txt files in "search_lists" folder (see example files)
+- run script
+- results will be saved as a csv file per search term.
+
+```
+python -m venv .venv
+. .venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python scrape.py
+```
+
+### Notes
+
+- you can have multiple txt files in the **search_lists** folder
+- One search term per line (no commas required)
+- You can change the **no_of_pages** results in **scrape.py**
+- You may need to tweak requests **header** in **scrape.py** for better results
+- Script doesn't seem to like search terms that are too broad.
+- Running the script multiple times will append the results to same file
+- ^ is used as the csv delimiter, You can split the text to columns in your favorite spreadsheet.
--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import datetime
@@ -6,37 +7,47 @@ import csv
 today = datetime.datetime.now()
 today = (today.strftime("%Y-%b-%d"))
 
-count = 0 # for no. of search results
+for root, _, files in os.walk("./search_lists", topdown=False):
+    for search_file in files:
+        full_search_file = os.path.join(root, search_file)
 
-query = 'wine hk' # the search query here
+        with open(full_search_file, 'r') as listfile:
+            lines = listfile.readlines()
 
-querys = query.replace(' ', '+')
-url = f'https://www.google.com/search?q={querys}'
+            for query in lines:
 
-query_title = query.replace(' ', '-') # used for filename
+                count = 0
+                no_of_pages = 2 # no of pages to search
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+                querys = query.replace(' ', '+')
+                url = f'https://www.google.com/search?q={querys}'
 
-with open(f'sem_{query_title}-{today}.csv', 'w', newline='') as csvfile:
-    linewriter = csv.writer(csvfile, delimiter='^', quotechar='"', quoting=csv.QUOTE_MINIMAL) # using ^ (caret) as delimiter
-    linewriter.writerow(['Rank', 'Title', 'Url', 'Description']) # write csv header line
+                query_title = query.replace(' ', '-') # used for filename
 
-    for i in range(3): # get results from x no. of pages
-        full_url = f'{url}&start={count}' # add results count to url (0=1st page, 10=2nd page etc)
-        result = requests.get(full_url, headers=headers)
+                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 
-        if result.status_code == 200:
-            soup = BeautifulSoup(result.content, 'lxml')
+                with open(f'sem_{query_title}.csv', 'a', newline='') as csvfile:
+                    linewriter = csv.writer(csvfile, delimiter='^', quotechar='"', quoting=csv.QUOTE_MINIMAL) # using ^ (caret) as delimiter
+                    linewriter.writerow(['Date', 'Rank', 'Title', 'Url', 'Description']) # write csv header line
 
-            search_results = soup.find_all('div', class_='rc') # the search results excluding paid ads
-            for index, result in enumerate(search_results, start = 1):
-                linewriter.writerow([f'{index}^ {result.h3.string}^ {result.a.get("href")}^ {result.h3.parent.parent.next_sibling.text}']) # write results in csv file
+                    for i in range(no_of_pages): # get results from x no. of pages
+                        full_url = f'{url}&start={count}' # add results count to url (0=1st page, 10=2nd page etc)
+                        result = requests.get(full_url, headers=headers)
 
-                # Print results out
-                print(f'Rank: {index}')
-                print(f'Title: {result.h3.string}')
-                print(f'Url: {result.a.get("href")}')
-                print(f'Description: {result.h3.parent.parent.next_sibling.text}')
-                print('############\n')
+                        if result.status_code == 200:
+                            soup = BeautifulSoup(result.content, 'lxml')
 
-        count = count + 10 # add 10 to get next 10 search results
+                            search_results = soup.find_all('div', class_='rc') # the search results excluding paid ads
+                            for index, result in enumerate(search_results, start = 1):
+                                index = index + count
+                                linewriter.writerow([f'{today}^ {index}^ {result.h3.string}^ {result.a.get("href")}^ {result.h3.parent.parent.next_sibling.text}']) # write results in csv file
+
+                                # Print results out
+                                print(f'Date: {today}')
+                                print(f'Rank: {index}')
+                                print(f'Title: {result.h3.string}')
+                                print(f'Url: {result.a.get("href")}')
+                                print(f'Description: {result.h3.parent.parent.next_sibling.text}')
+                                print('############\n')
+
+                        count = count + 10 # add 10 to get next 10 search results
